3c4bfb2dbd339e39fac83f578d9b5b828d4640ae
xlc <xlc@toys178.com>
2016-05-19 12:16:18 +0800

    修改E签宝正式环境参数。

--- shop/config/esign.config.ini.php
+++ shop/config/esign.config.ini.php
@@ -1,11 +1,11 @@
 <?php
 //e签宝的配置信息文件 - 测试
-const E_PROJECT_ID = "1111563517";
-const E_PROJECT_SECRET = "95439b0863c241c63a861b87d1e647b7";
+//const E_PROJECT_ID = "1111563517";
+//const E_PROJECT_SECRET = "95439b0863c241c63a861b87d1e647b7";
 
 
 //e签宝的配置信息文件 - 正式
-//const E_PROJECT_ID = "1111563606";
-//const E_PROJECT_SECRET = "95439b0863c241c63a861b87d1e647b7";
+const E_PROJECT_ID = "1111563606";
+const E_PROJECT_SECRET = "3bce4b8dcb911e740b31debcfdf1419a";
 ?>
 
